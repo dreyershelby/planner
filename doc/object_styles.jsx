018c79be2ca609d67c2710903687ff8b10951632
dreyershelby <dreyershelby@gmail.com>
2019-05-26 23:13:35 -0700

    say fuck it n do objectStyle.basedOn by hand
    why did adobe take a shit and call it a working library/compiler

--- doc/object_styles.jsx
+++ doc/object_styles.jsx
@@ -5,14 +5,14 @@ var doc = app.activeDocument;
 
 const txt_box_dimensions = 1; // 1p0
 
-var base = ( doc.objectStyles.itemByName("base")
-             || doc.objectStyles.add() );
-base.properties = {
+var base = (doc.objectStyles.itemByName("base") != null)
+              ? doc.objectStyles.itemByName("base")
+              : doc.objectStyles.add();
+var base_prop = base.properties = {
   name : "base",
 
   enableFill                    : true,
   enableParagraphStyle          : true,
-  enableStroke                  : true,
   enableStrokeAndCornerOptions  : true,
   enableTextFrameGeneralOptions : true,
 
@@ -25,44 +25,49 @@ base.properties = {
   topRightCornerOption : CornerOptions.NONE
 };
 
-var pg_num = ( doc.objectStyles.itemByName("pg_num")
-               || doc.objectStyles.add() );
+var pg_num = (doc.objectStyles.itemByName("pg_num") != null)
+                ? doc.objectStyles.itemByName("pg_num")
+                : doc.objectStyles.add();
+pg_num.properties = base_prop;
 pg_num.properties = {
   name : "pg_num",
-  basedOn : base,
   appliedParagraphStyle : doc.paragraphStyles.itemByName("marginal_nobind"),
-  textFramePreferences : {
+  textFramePreferences  : {
     verticalJustification : VerticalJustification.BOTTOM_ALIGN
   }
 };
 
-var week_num = ( doc.objectStyles.itemByName("week_num")
-                 || doc.objectStyles.add() );
+var week_num = (doc.objectStyles.itemByName("week_num") != null)
+                  ? doc.objectStyles.itemByName("week_num")
+                  : doc.objectStyles.add();
+week_num.properties = base_prop;
 week_num.properties = {
   name : "week_num",
-  basedOn : base,
   appliedParagraphStyle : doc.paragraphStyles.itemByName("marginal_bind"),
   textFramePreferences : {
     verticalJustification : VerticalJustification.TOP_ALIGN
   }
 };
 
-var date_range = ( doc.objectStyles.itemByName("date_range")
-                   || doc.objectStyles.add() );
+var date_range = (doc.objectStyles.itemByName("date_range") != null)
+                    ? doc.objectStyles.itemByName("date_range")
+                    : doc.objectStyles.add();
+date_range.properties = base_prop;
 date_range.properties = {
   name : "date_range",
-  basedOn : base,
   appliedParagraphStyle : doc.paragraphStyles.itemByName("marginal_nobind"),
   textFramePreferences : {
     verticalJustification : VerticalJustification.TOP_ALIGN
   }
 };
 
-var calendar_heading = ( doc.objectStyles.itemByName("calendar_heading")
-                         || doc.objectStyles.add() );
+var calendar_heading =
+    (doc.objectStyles.itemByName("calendar_heading") != null)
+       ? doc.objectStyles.itemByName("calendar_heading")
+       : doc.objectStyles.add();
+calendar_heading.properties = base_prop;
 calendar_heading.properties = {
   name : "calendar_heading",
-  basedOn : base,
   appliedParagraphStyle : doc.paragraphStyles.itemByName("obj_heading"),
   textFramePreferences : {
     verticalJustification : VerticalJustification.BOTTOM_ALIGN,
