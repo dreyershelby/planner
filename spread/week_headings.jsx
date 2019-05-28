b8b9493b99f300ebfc472d5dc1305641a9543f9b
dreyershelby <dreyershelby@gmail.com>
2019-05-27 17:57:09 -0700

    get week_headings.jsx working as expected
    previous difficulties included:
    - week headings for 2nd page in spread were being placed over the 1st
    page's headings or not being placed at all because the boxes' bounds
    were referencing bounds for a 1st page

--- spread/week_headings.jsx
+++ spread/week_headings.jsx
@@ -23,7 +23,7 @@ function week_dates() {
   var sun = new Date();
   sun.setDate(sun.getDate() - sun.getDay());
   var sat = new Date(sun);
-  sat.setDate(sat.getDate() + 7);
+  sat.setDate(sat.getDate() + 6);
 
   var str = "";
   if (sun.getMonth() == sat.getMonth()) {
@@ -41,32 +41,31 @@ function week_dates() {
   return str;
 }
 
-function add_to(pg) {
-  // constants
+function add_to(pg, on_doc) {
   const txt_box_height = 1; // 1p0
 
   const week_str = "week " + week_num_in_year();
   var week_box = pg.textFrames.add({
-    itemLayer          : doc.layers.itemByName("marginals"),
-    appliedObjectStyle : doc.objectStyles.itemByName("week_num"),
+    itemLayer          : on_doc.layers.itemByName("marginals"),
+    appliedObjectStyle : on_doc.objectStyles.itemByName("week_num"),
     contents           : week_str
   })
 
   const date_range_str = week_dates();
   var date_range_box = pg.textFrames.add({
-    itemLayer          : doc.layers.itemByName("marginals"),
-    appliedObjectStyle : doc.objectStyles.itemByName("date_range"),
+    itemLayer          : on_doc.layers.itemByName("marginals"),
+    appliedObjectStyle : on_doc.objectStyles.itemByName("date_range"),
     contents           : date_range_str,
   })
 
-  const left_box  = [ pg.bounds[0],                  // y1
-                      pg.bounds[1],                  // x1
-                      pg.bounds[0] + txt_box_height, // y2
-                      pg.bounds[3] / 2 ];            // x2
-  const right_box = [ pg.bounds[0],                  // y1
-                      pg.bounds[3] / 2,              // x1
-                      pg.bounds[0] + txt_box_height, // y2
-                      pg.bounds[3] ];                // x2
+  const left_box  = [ pg.bounds[0],                       // y1
+                      pg.bounds[1],                       // x1
+                      pg.bounds[0] + txt_box_height,      // y2
+                     (pg.bounds[3] + pg.bounds[1]) / 2 ]; // x2
+  const right_box = [ pg.bounds[0],                       // y1
+                     (pg.bounds[3] + pg.bounds[1]) / 2,   // x1
+                      pg.bounds[0] + txt_box_height,      // y2
+                      pg.bounds[3] ];                     // x2
   if (pg.side == PageSideOptions.LEFT_HAND) {
     week_box.geometricBounds       = right_box;
     date_range_box.geometricBounds = left_box;
@@ -76,10 +75,11 @@ function add_to(pg) {
   }
 }
 
+var doc    = app.activeDocument;
 var spread = app.layoutWindows.item(0).activeSpread;
-var page = null;
+var page   = null;
 
 for (var pg_index = 0; pg_index < spread.pages.length; pg_index++) {
   page = spread.pages.item(pg_index);
-  add_to(page);
+  add_to(page, doc);
 }
